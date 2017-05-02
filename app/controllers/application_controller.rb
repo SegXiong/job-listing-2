213ae0a3c23533c421e923ce9d99e6f3f1fa0831
SegueX <free1989xj@hotmail.com>
2017-05-02 17:51:57 +0800

    fix login issue & toolbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :store_current_location, :unless => :devise_controller?
   protect_from_forgery with: :exception
 
   def require_is_admin
@@ -9,4 +10,15 @@ class ApplicationController < ActionController::Base
     end
 
   end
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end
